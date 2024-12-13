4a5efb4c87f255977dea5a48d3739e8484c7942d
Mikhail <m.ataev@mail.ru>
2024-12-13 15:27:35 +0100

    Update package.json and add server.js for Render deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/kniga-app/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default Render port
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
