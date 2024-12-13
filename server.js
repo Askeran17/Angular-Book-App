fb72f802fd916153d8b39417cea5b29dae171438
Mikhail <m.ataev@mail.ru>
2024-12-13 16:06:53 +0100

    Update server.js for Render deployment

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, 'dist/kniga-app')));
+// Serve static files from the src directory
+app.use(express.static(path.join(__dirname, 'src')));
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/kniga-app/index.html'));
+  res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
 // Start the app by listening on the default Render port
@@ -15,3 +15,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
