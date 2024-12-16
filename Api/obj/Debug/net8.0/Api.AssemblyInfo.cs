8f9c595b9d23b50e2daf4952dccdb8b4af62ff7d
Mikhail <m.ataev@mail.ru>
2024-12-16 20:55:01 +0100

    ff

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295309372cdd843de1f1df4c8ca30d2dfefa713c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b5b2580f9cdf3f955e33f71dc72592079c2b52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
