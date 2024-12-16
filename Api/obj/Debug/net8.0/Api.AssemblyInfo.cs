295309372cdd843de1f1df4c8ca30d2dfefa713c
Mikhail <m.ataev@mail.ru>
2024-12-16 20:44:34 +0100

    add

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3699f599582e765c7807bcd1b0ddb0b1af6ab6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb36d746d04a90da8e4c7fae5c274d0119351dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -35,7 +34,7 @@ app.post('/api/auth/login', (req, res) => {
   const { username, password } = req.body;
   const user = users.find(u => u.username === username);
   if (user && bcrypt.compareSync(password, user.password)) {
-    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET);
+    const token = jwt.sign({ username: user.username }, 'your_jwt_secret');
     res.json({ token });
   } else {
     res.status(401).send('Invalid credentials');
@@ -56,13 +55,13 @@ app.get('/api/protected', (req, res) => {
 
 // Proxy API requests to .NET Core server
 app.use('/api', createProxyMiddleware({
-  target: process.env.API_URL || 'http://localhost:5089',
-  changeOrigin: true,
-  logLevel: 'debug',
-  onError: (err, req, res) => {
-    console.error('Proxy error:', err);
-    res.status(500).send('Proxy error');
-  }
+    target: process.env.API_URL || 'http://localhost:5089/api', // URL вашего .NET 8 сервера
+    changeOrigin: true,
+    logLevel: 'debug', // Добавьте это для логирования
+    onError: (err, req, res) => {
+        console.error('Proxy error:', err);
+        res.status(500).send('Proxy error');
+    }
 }));
 
 // Catch all other routes and return the index file
@@ -70,13 +69,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'Api/wwwroot/index.html'));
 });
 
-// Start the app by listening on the default port
+// Start the app by listening on the default Heroku port
 const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(`Environment: ${process.env.NODE_ENV}`);
-  console.log(`ASPNETCORE_ENVIRONMENT: ${process.env.ASPNETCORE_ENVIRONMENT}`);
 });
+
+
 
 
