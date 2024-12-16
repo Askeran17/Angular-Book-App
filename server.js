02d5f3b38105e492307c922ab782739dbb2a0f3f
Mikhail <m.ataev@mail.ru>
2024-12-16 05:04:21 +0100

    hh

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const compression = require('compression');
+const http = require('http');
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(compression());
 
 // Serve static files from the Api/wwwroot directory
 app.use(express.static(path.join(__dirname, 'Api/wwwroot')));
@@ -66,9 +69,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'Api/wwwroot/index.html'));
 });
 
-// Start the app by listening on the default Render port
+// Start the app by listening on the default Heroku port
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const server = http.createServer(app);
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
