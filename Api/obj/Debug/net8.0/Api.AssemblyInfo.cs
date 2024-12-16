de7e533734b5ccea5936378e3ffaa30d0041a5cb
Mikhail <m.ataev@mail.ru>
2024-12-16 19:46:50 +0100

    Update environment.prod.ts with correct API URL

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00424e2fe90a2a148395297e4a1d2480f85f7f9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce44624c2bb509360329c8fd0fecdb06ea83a91e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
